b2fec71170517c9d3b82a6e1ca2659c69d833352
samarvir singh <samar@samarvirs-MacBook-Air.local>
2018-02-23 14:23:53 +0530

    v1 works but has quirks

---
+++ logisticRegression.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd 
+import pdb
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import scipy
+from PIL import Image
+from scipy import ndimage
+
+%matplotlib inline
+
+dataset = 
