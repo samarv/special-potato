1837c0f5cb2063599fb71cd33ef419ef60a1233b
samarvir singh <samar@samarvirs-MacBook-Air.local>
2018-02-23 14:31:36 +0530

    v1 works but has quirks

--- logisticRegression.py
+++ logisticRegression.py
@@ -1,7 +1,6 @@
-import numpy as np
-import pandas as pd 
 import pdb
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import h5py
 import scipy
@@ -10,4 +9,147 @@ from scipy import ndimage
 
 %matplotlib inline
 
-dataset = 
+dataset = pd.read_csv("/Users/samar/Documents/pythonwd/special-potato/AXISBANK.csv")
+dataset["t1"] = dataset["4. close"].shift(-1)
+dataset = dataset[0:(dataset.shape[0]-1)]
+dataset = dataset[["4. close", "t1"]]
+
+
+#train test slpit boyss
+
+split = int((len(dataset)* 2)/3)
+train = dataset[:split]
+test = dataset[split:]
+train = train.values
+test = test.values
+train_x = train.T[0]
+train_y = train.T[1]
+test_x = test.T[0]
+test_y = test.T[1]
+m_train = train_x.shape[0]
+m_test = test_x.shape[0]
+
+train_set_x = norm(train_x.reshape(m_train, -1).T)
+test_set_x = norm(test_x.reshape(m_test, -1).T)
+train_set_y = norm(train_y.reshape(m_train, -1).T)
+test_set_y = norm(test_y.reshape(m_test, -1).T)
+
+np.mean(train_set_x)
+
+
+def norm(n):
+    n1 = n/np.mean(n)
+    return n1
+    
+def denorm(n):
+    n2 = n * np.mean(n)
+    return n2
+
+ee = norm(train_set_x)
+def sigmoid(z):
+    s = 1/(1+ np.exp(-(z)))
+    return s
+    
+def initialize_with_zeros(dim):
+    w = np.zeros((dim, 1))
+    b = 0
+    return w,b
+
+def propagate(w, b, X, Y):
+    m = X.shape[1]
+    A = sigmoid(np.dot(w.T, X) + b)                               
+    cost = (- 1 / m) * np.sum(Y * np.log(A) + (1 - Y) * (np.log(1 - A)))                 
+    dw = (1 / m) * np.dot(X, (A - Y).T)
+    db = (1 / m) * np.sum(A - Y)
+    cost = np.squeeze(cost)
+    grads = {"dw": dw,
+             "db": db}
+    
+    return grads, cost
+
+
+
+def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
+    costs = []
+    for i in range(num_iterations):
+        grads, cost = propagate(w, b, X, Y)
+        dw = grads["dw"]
+        db = grads["db"]
+        w = w - learning_rate * dw  
+        b = b - learning_rate * db
+        if i % 100 == 0:
+            costs.append(cost)
+        if print_cost and i % 100 == 0:
+            print ("Cost after iteration %i: %f" % (i, cost))
+    
+    params = {"w": w,
+              "b": b}
+    
+    grads = {"dw": dw,
+             "db": db}
+    
+    return params, grads, costs
+    
+def predict(w, b, X):
+    m = X.shape[1]
+    Y_prediction = np.zeros((1, m))
+    w = w.reshape(X.shape[0], 1)
+    A = sigmoid(np.dot(w.T, X) + b)
+    for i in range(A.shape[1]):
+        Y_prediction[0, i] = 1 if A[0, i] > 0.5 else 0
+    return Y_prediction
+    
+    
+def model(X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0.5, print_cost=False):
+    w, b = initialize_with_zeros(X_train.shape[0])
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
+    w = parameters["w"]
+    b = parameters["b"]
+    Y_prediction_test = predict(w, b, X_test)
+    Y_prediction_train = predict(w, b, X_train)
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+    d = {"costs": costs,
+         "Y_prediction_test": Y_prediction_test, 
+         "Y_prediction_train" : Y_prediction_train, 
+         "w" : w, 
+         "b" : b,
+         "learning_rate" : learning_rate,
+         "num_iterations": num_iterations}
+    return d
+
+d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 2000, learning_rate = 0.005, print_cost = True)
+
+learning_rates = [0.01, 0.001, 0.0001]
+models = {}
+for i in learning_rates:
+    print ("learning rate is: " + str(i))
+    models[str(i)] = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 1500, learning_rate = i, print_cost = False)
+    print ('\n' + "-------------------------------------------------------" + '\n')
+
+for i in learning_rates:
+    plt.plot(np.squeeze(models[str(i)]["costs"]), label= str(models[str(i)]["learning_rate"]))
+
+plt.ylabel('cost')
+plt.xlabel('iterations')
+
+legend = plt.legend(loc='upper center', shadow=True)
+frame = legend.get_frame()
+frame.set_facecolor('0.90')
+plt.show()
+
+costs = np.squeeze(d['costs'])
+plt.plot(costs)
+plt.ylabel('cost')
+plt.xlabel('iterations (per hundreds)')
+plt.title("Learning rate =" + str(d["learning_rate"]))
+plt.show()
+
+
+plt.plot(denorm(d["Y_prediction_test"]))
+plt.plot(d["Y_prediction_train"])
+plt.show()
+
+
+denorm(d["Y_prediction_test"])
+
